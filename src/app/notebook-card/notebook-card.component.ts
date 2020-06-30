fb6a8c712a579e52133bf06f372d49b74ea91c97
Ítalo <italoberg@yahoo.com.br>
2020-06-30 00:43:35 -0300

    first layout

---
+++ src/app/notebook-card/notebook-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notebook-card',
+  templateUrl: './notebook-card.component.html',
+  styleUrls: ['./notebook-card.component.css']
+})
+export class NotebookCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
